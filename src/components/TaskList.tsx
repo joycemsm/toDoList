6c0c50b05fe5ebb3fe227b10b825ddd5c25cf98c
Joyce <joycemara.sm@gmail.com>
2021-03-15 14:05:49 +0100

    Creat function to add, check and delet tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,18 +15,18 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle?.length > 0) {
     setTasks([...tasks,{
       id: Math.floor(Math.random() * 100), 
       title: newTaskTitle,
       isComplete: false
     }])
     setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-
     const findIndex = tasks.findIndex(e => e.id === id)
 
     if (findIndex !== -1) {
@@ -44,12 +44,10 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     const tasksWithoutID = tasks.filter(e => e.id !== id)
     setTasks(tasksWithoutID)
   }
  
-
   return (
     <section className="task-list container">
       <header>
